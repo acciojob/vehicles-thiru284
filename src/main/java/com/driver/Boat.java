9cebaeb7730198839e5f08947a55868486ffd586
Thirumoorthy Velumani <thirumoorthy@mallow-tech.com>
2023-02-14 21:04:55 +0530

    soln

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,7 +3,10 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
-
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
     public String getName() {
         return name;
     }
